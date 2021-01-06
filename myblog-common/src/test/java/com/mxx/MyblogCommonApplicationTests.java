c34ab05a6794b2dde3d944a5b15257c23697d523
maoxinxin <1625183368@qq.com>
2021-01-06 16:19:16 +0800

    day1test

---
+++ myblog-common/src/test/java/com/mxx/MyblogCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
